393152ed83577f0d449899d0047ad0145d39a659
prashant1786 <prashantsuthar.sp.00@gmail.com>
2024-07-14 08:08:29 +0530

    Task-2 completed

---
+++ Day-2/task-2.js
@@ -0,0 +1,61 @@
+// ------------------------------------------------------------------------Activity-1------------------------------------------------------------------------
+
+console.log(" ");
+// Arithmetic Operation
+
+let num_1=10,num_2=5,num_3=8;
+
+
+console.log(`addition of num_1 and num_2(10,5) : ${num_1+num_2}`)
+console.log(`subtraction of num_1 and num_2(10,5) : ${num_1-num_2}`)
+console.log(`multiplication of num_1 and num_2(10,5) : ${num_1*num_2}`)
+console.log(`divison of num_1 and num_2(10,5) : ${num_1/num_2}`)
+console.log(`reminder of num_3 and num_2(8,5) : ${num_3%num_2}\n`)
+
+// ------------------------------------------------------------------------Activity-2------------------------------------------------------------------------
+
+
+//Assignment Operators
+console.log(`+= on num_3 and 5 : ${num_3+=5}`);
+console.log(`-= on num_3 and 7 : ${num_3-=7}\n`);
+
+// ------------------------------------------------------------------------Activity-3------------------------------------------------------------------------
+
+//Comparison Operators
+
+console.log(`is 3 > 5 :${3>5}`);
+console.log(`is 3 < 5 :${3<5}`);
+console.log(`is 6 >= 5 :${6>=5}`);
+console.log(`is 6 <= 5 :${6<=5}`);
+console.log(`is "5" == 5 :${"5"==5}`);
+console.log(`is "5" === 5 :${"5"===5}\n`);
+
+
+
+// ------------------------------------------------------------------------Activity-4------------------------------------------------------------------------
+
+//Logical Operators
+console.log(`logical AND operator(&&)\n`);
+console.log(`true && true : ${true && true}`);
+console.log(`true && false : ${true && false}`);
+console.log(`false && true : ${false && true}`);
+console.log(`false && false : ${false && false}\n`);
+
+console.log(`logical OR operator(||)\n`);
+console.log(`true || true : ${true || true}`);
+console.log(`true || false : ${true || false}`);
+console.log(`false || true : ${false || true}`);
+console.log(`false || false : ${false || false}\n`);
+
+
+console.log(`logical NOT operator(!)\n`);
+console.log(`!true : ${!true}`);
+console.log(`!false : ${!false}\n`);
+
+// ------------------------------------------------------------------------Activity-5------------------------------------------------------------------------
+
+//Ternary Operators
+
+console.log(`ternary Operator`)
+console.log(`is 5 postive/negative number : ${(5>0)?"Positive":"Negative"}`)
+console.log(`is -5 postive/negative number : ${(-5>0)?"Positive":"Negative"}\n`)
